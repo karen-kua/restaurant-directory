151d40a864beec324938a65a925f697fef011649
Durian <karen.kua7@gmail.com>
2019-02-01 22:57:33 -0500

    Updates

---
+++ precache-manifest.52faf96cac21aae5c394e78e2aaf9adb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4deaa2254ba69d3600a5",
+    "url": "/restaurant-directory/static/js/runtime~main.4deaa225.js"
+  },
+  {
+    "revision": "aef7509ce0e224a50fb8",
+    "url": "/restaurant-directory/static/js/main.aef7509c.chunk.js"
+  },
+  {
+    "revision": "7dda037a1f09902c8754",
+    "url": "/restaurant-directory/static/js/1.7dda037a.chunk.js"
+  },
+  {
+    "revision": "aef7509ce0e224a50fb8",
+    "url": "/restaurant-directory/static/css/main.8776bdcd.chunk.css"
+  },
+  {
+    "revision": "f271f1f3fc5820ed4e3575eec8fe5241",
+    "url": "/restaurant-directory/index.html"
+  }
+];
